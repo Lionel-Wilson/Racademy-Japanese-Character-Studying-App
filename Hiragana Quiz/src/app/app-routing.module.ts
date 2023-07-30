8e10d91c53a5c743bfd9262986a82103998683eb
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-07-30 22:21:34 +0100

    Add routing to welcome and question components

--- Hiragana Quiz/src/app/app-routing.module.ts
+++ Hiragana Quiz/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './quiz/welcome/welcome.component';
+import { QuestionComponent } from './quiz/question/question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:"Welcome",pathMatch:"full"},
+  {path:"Welcome",component:WelcomeComponent},
+  {path:"Question",component:QuestionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Hiragana Quiz/src/app/quiz/welcome/welcome.component.ts
+++ Hiragana Quiz/src/app/quiz/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  public data:QuizItem[]=[];
+  public numberOfQuestions:number = 0; 
+  public configUrl = 'https://localhost:7013/Quiz';
 
+  constructor(private http: HttpClient) { }
+
+  public getData(numberOfQuestionSetByUser:any){
+    this.http.get<QuizItem[]>(this.configUrl,{params:numberOfQuestionSetByUser}).subscribe((results => {
+      this.data = results;
+      this.numberOfQuestions = this.data.length;
+    }))
+  }
+
+  public getQuestions( numberOfQuestionSetByUser: NgForm ){
+    this.getData(numberOfQuestionSetByUser.value);
+  }
+}
+
+
+
+interface QuizItem {
+  id: string;
+  hiragana: string;
+  answer: string;
 }
