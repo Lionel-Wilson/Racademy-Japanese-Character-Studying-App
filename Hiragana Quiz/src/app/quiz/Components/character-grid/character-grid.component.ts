a0034f2787a84b3bd2d5cb5ecc5bdc918ac86d7c
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-08-16 23:26:50 +0100

    make grid highlight when selected

--- Hiragana Quiz/src/app/quiz/Components/character-grid/character-grid.component.ts
+++ Hiragana Quiz/src/app/quiz/Components/character-grid/character-grid.component.ts
@@ -15,6 +15,11 @@ export class CharacterGridComponent {
   public charactersToBeTestedOn: Character[]=[];
   public endCharacters: string[] = ["o","ko","so","to","no","ho","mo","yo","ro","wo","-n","go","zo","do","bo","po","vu","kyo","syo","tyo","nyo","hyo","myo","ryo","gyo","zyo","dyo","zyo","byo","byo"];
 
+  public columnSelectedColour:Boolean = false;
+
+  public selectedColumnIndex=-1;
+  public selectedColumn :Character[]=[];  
+
  
   constructor(private DataService:DataService){
     this.DataService.Characters.subscribe(response=>{
@@ -50,13 +55,24 @@ export class CharacterGridComponent {
 
 
  public columnSelected(column: Character[],index:number){
+
+  if(this.columnSelectedColour){
+    this.selectedColumnIndex = -1;
+    this.selectedColumn = [];
+    this.columnSelectedColour =false;
+  }
+  else{
+    this.selectedColumn = column;
+    this.selectedColumnIndex = index;
+    this.columnSelectedColour =true;
+  }
   console.log(column);
   console.log(index);
   column.forEach(element => {
     this.charactersToBeTestedOn.push(element);
   });
 
-  console.log(this.charactersToBeTestedOn);
+ 
 
 }
 
